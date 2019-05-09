eddf3cbf24a49546c02774bf875d7b3da78842e4
Mr_Diamond123 <kurtisiscool123@gmail.com>
2019-05-09 13:30:51 -0400

    Update polygon.js

--- scripts/polygon.js
+++ scripts/polygon.js
@@ -1,21 +1,34 @@
-var ctx = document.getElementById('polygon').getContext('2d');
+const canvas = document.getElementById('polygon');
+const ctx = canvas.getContext('2d');
+
+var polygonSides = document.getElementById('polygonSides');
 
 var numberOfSides = 6,
-    size = 20,
+    size = 50,
     Xcenter = 25,
     Ycenter = 25;
 
-ctx.beginPath();
-ctx.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
+function makePolygon() {
+    ctx.beginPath();
+    ctx.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
 
-for (var i = 1; i <= numberOfSides;i += 1) {
-  ctx.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+    for (var i = 1; i <= numberOfSides;i += 1) {
+    ctx.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+    }
+
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 1;
+    ctx.stroke();
 }
 
-ctx.strokeStyle = "#000000";
-ctx.lineWidth = 1;
-ctx.stroke();
+function changePolygon() {
+    numberOfSides = polygonSides.value();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    makePolygon();
+}
 
+polygonSides.value = numberOfSides;
+makePolygon();
 
 
 //Thanks stackoverflow :D
