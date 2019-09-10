e9746f6865817da2b08de787acf2fa8e9ef498ed
Mr_Diamond123 <kurtisiscool123@gmail.com>
2019-09-10 11:27:43 -0400

    Update math.js

--- scripts/math.js
+++ scripts/math.js
@@ -199,4 +199,15 @@ input.addEventListener("keyup", function(event) {
   }
 });
 
+// Add code to enable cheat bu†ton by clicking the title
+document.addEventListener('click', function (event) {
 
+	// If the clicked element doesn't have the right selector, bail
+	if (!event.target.matches('.title')) return;
+
+	// Don't follow the link
+	event.preventDefault();
+
+	// Log the clicked element in the console
+	console.log(event.target);
+}, false);
